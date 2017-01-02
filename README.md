5409bbb5b63a23b25101d02635bc193c6899eb3f
Pan Wenbin <panwenbin@gmail.com>
2017-01-02 16:10:09 +0800

    fix loosing use statment of Exception change stability to stable change namespace and package name for publishing

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
-# yii2-eithervalidator
-Yii2 validator to force one of attributes to be filled.
+# yii2-either-validator
+Yii2 validator for either attributes is required
 
 ## Installation
 
-Add the following line to the composer.
-
 ```
-"petrabarus/yii2-eithervalidator": "*"
+composer require "panwenbin/yii2-eithervalidator"
 ```
 
 ## Usage
 
-Add the a line similar to the following to rule of the model
+Add a rule similar to the following to rules of the model
 
 ```php
-[['email'], PetraBarus\Yii2\Validators\EitherValidator::class, 
-	'otherAttributes' => ['phone']]
+    [
+        ['email'],
+        EitherValidator::className(),
+        'eitherAttributes' => ['phone'],
+        'message' => Yii::t('app', 'Either attribute {attribute}, {either_attributes} is required')
+    ]
 ```
--- src/EitherValidator.php
+++ src/EitherValidator.php
@@ -1,35 +1,33 @@
 <?php
-
 /**
  * EitherValidator class file.
- * 
+ *
  * @author Petra Barus <petra.barus@gmail.com>
  * @since 2015.02.12
  */
 
-namespace PetraBarus\Yii2\Validators;
+namespace panwenbin\yii2\validators;
 
 use Yii;
+use yii\base\InvalidConfigException;
 use yii\helpers\Html;
 
 /**
- * EitherValidator forces one of attribute in numbers of attributes have to be
- * filled.
- * 
- * To use this
- * 
+ * EitherValidator forces either the attribute or any attribute in eitherAttributes is required
+ *
+ * To use like this
+ *
  * ```
- *    [['email', EitherValidator::class, 
- *          'otherAttributes' => ['phone'], 
- *          'message' => Yii::t('app', 'Either attribute {attribute}, {other_attribute} is required')
+ *    [
+ *        ['email'],
+ *        EitherValidator::className(),
+ *        'eitherAttributes' => ['phone'],
+ *        'message' => Yii::t('app', 'Either attribute {attribute}, {either_attributes} is required')
  *    ]
  * ```
- * 
- * @author Petra Barus <petra.barus@gmail.com>
- * @since 2015.02.12
  */
-class EitherValidator extends \yii\validators\Validator {
-
+class EitherValidator extends \yii\validators\Validator
+{
     /**
      * The error message.
      * @var string
@@ -37,10 +35,10 @@ class EitherValidator extends \yii\validators\Validator {
     public $message;
 
     /**
-     * List of other attributes that have to be filled.
+     * List of either attributes that either have to be filled.
      * @var array
      */
-    public $otherAttributes = [];
+    public $eitherAttributes = [];
 
     /**
      * This has to set as false or it defeats the purpose.
@@ -48,12 +46,13 @@ class EitherValidator extends \yii\validators\Validator {
      */
     public $skipOnEmpty = false;
 
-    public function init() {
-        if (empty($this->otherAttributes)) {
-            throw new Exception(Yii::t('yii', 'Other attributes are not set'));
+    public function init()
+    {
+        if (empty($this->eitherAttributes)) {
+            throw new InvalidConfigException(Yii::t('yii', 'EitherAttributes are not set'));
         }
         if (!isset($this->message)) {
-            $this->message = Yii::t('yii', 'Either \'{attribute}\', \'{other_attributes}\' has to be filled');
+            $this->message = Yii::t('yii', 'Either \'{attribute}\', \'{either_attributes}\' has to be filled');
         }
     }
 
@@ -61,15 +60,17 @@ class EitherValidator extends \yii\validators\Validator {
      * @param \yii\base\Model $model
      * @param string $attribute the name of the attribute.
      */
-    public function validateAttribute($model, $attribute) {
+    public function validateAttribute($model, $attribute)
+    {
         $values = [];
         $values[$attribute] = $model->{$attribute};
-        foreach ($this->otherAttributes as $otherAttribute) {
-            $values[$otherAttribute] = $model->{$otherAttribute};
+        foreach ($this->eitherAttributes as $eitherAttribute) {
+            $values[$eitherAttribute] = $model->{$eitherAttribute};
         }
-        if (count(array_filter($values, function($e) {
-                     return !isset($e) || strlen($e) > 0;
-                })) == 0) {
+        $filledValues = array_filter($values, function ($value) {
+            return !$this->isEmpty($value);
+        });
+        if (count($filledValues) == 0) {
             $this->addError($model, $attribute, $this->message, $this->getErrorParams($model, $attribute));
         }
     }
@@ -77,13 +78,16 @@ class EitherValidator extends \yii\validators\Validator {
     /**
      * @param \yii\base\Model $model the model to be validated.
      * @param string $attribute the name of the attribute to be validated.
+     * @return array
      */
-    private function getErrorParams($model, $attribute) {
+    private function getErrorParams($model, $attribute)
+    {
+        $eitherAttributeLabels = array_map(function ($eitherAttribute) use ($model) {
+            return $model->getAttributeLabel($eitherAttribute);
+        }, $this->eitherAttributes);
         return [
             'attribute' => $model->getAttributeLabel($attribute),
-            'other_attributes' => implode(', ', array_map(function($e) use($model) {
-                                      return $model->getAttributeLabel($e);
-                            }, $this->otherAttributes)),
+            'either_attributes' => implode(', ', $eitherAttributeLabels),
         ];
     }
 
@@ -91,24 +95,26 @@ class EitherValidator extends \yii\validators\Validator {
      * @param \yii\base\Model $model the model to be validated.
      * @param string $attribute the name of the attribute to be validated.
      * @param \yii\web\View $view the view.
+     * @return string
      */
-    public function clientValidateAttribute($model, $attribute, $view) {
+    public function clientValidateAttribute($model, $attribute, $view)
+    {
         $options = [
             'attribute' => Html::getInputId($model, $attribute),
         ];
-        foreach ($this->otherAttributes as $otherAttribute) {
-            $options['otherAttributes'][] = Html::getInputId($model, $otherAttribute);
+        foreach ($this->eitherAttributes as $eitherAttribute) {
+            $options['eitherAttributes'][] = Html::getInputId($model, $eitherAttribute);
         }
         $options['message'] = Yii::$app->getI18n()->format($this->message,
-                $this->getErrorParams($model, $attribute), Yii::$app->language);
+            $this->getErrorParams($model, $attribute), Yii::$app->language);
         $optionJson = json_encode($options, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
         return <<<JS
 (function(){
     var options = {$optionJson};
     var values = [];
     values.push($('#' + options.attribute).val());
-    for (i in options.otherAttributes) {
-        values.push($('#' + options.otherAttributes[i]).val());
+    for (var i in options.eitherAttributes) {
+        values.push($('#' + options.eitherAttributes[i]).val());
     }
     if (values.filter(function(e){
             return e.length > 0;
